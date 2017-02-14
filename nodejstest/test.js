28271fa4a380475147314c98cac144d0d6807342
houxl <houxl@git.com>
2017-02-14 10:34:54 +0800

    11

--- nodejstest/test.js
+++ nodejstest/test.js
@@ -1,7 +1,17 @@
 var http = require("http");
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hello World!");
-    response.end();
-}).listen(8088);
-console.log("Server running at http://localhost:8088/");
+var fs = require("fs");
+//参考http://www.jb51.net/article/29855.htm
+exports.start = function() {
+    http.createServer(function (request, response) {
+        fs.readFile('./index.html', 'utf-8', function (err, data) {//读取内容
+            if (err) throw err;
+            response.writeHead(200, {"Content-Type": "text/html"});//注意这里
+            response.write(data);
+            response.end();
+            /*  response.writeHead(200, {"Content-Type": "text/html"});
+              response.write("Hello World!");
+              response.end();*/
+        });
+    }).listen(8088);
+    console.log("Server running at http://localhost:8088/");
+}
